0118e702841c0aa18517f54d90cf6201f12cfaf8
Arash Milani <me@arashmilani.com>
2017-04-19 14:26:54 +0430

    added initial functional src

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = store => next => action => {
+  if(!action.promise || typeof action.promise.then !== 'function') {
+    return next(action)
+  }
+
+  let actionType = []
+
+  if(typeof action.type === 'string') {
+    actionType = [action.type + '_INIT', action.type + '_SUCCESS', action.type + '_ERROR']
+  } else if(Array.isArray(action.type)) {
+    if(action.type.length !== 3) {
+      throw new Error('Length of action type for async actions should be three.')
+    }
+    actionType = [...action.type]
+  } else {
+    throw new Error('Action type should be either a string or an array.')
+  }
+
+  if(Array.isArray(actionType[0])) {
+    store.dispatch(Object.assign({type: actionType[0][0]},  actionType[0][1]))
+  } else {
+    store.dispatch({type: actionType[0]})
+  }
+
+  action.promise.then(payload => {
+    store.dispatch({type: actionType[1], payload})
+  }).catch(error => {
+    store.dispatch({type: actionType[2], error})
+  })
+}
