fda3180a8e9f72a6fd9356bc56b21ad720bad57a
Arash Milani <me@arashmilani.com>
2017-08-22 15:48:11 +0430

    added init payload and done functions

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 module.exports = store => next => action => {
+  if (typeof action === 'function') {
+    return action(store.dispatch, store.getState)
+  }
+
   if(!action.promise || typeof action.promise.then !== 'function') {
     return next(action)
   }
@@ -16,14 +20,19 @@ module.exports = store => next => action => {
     throw new Error('Action type should be either a string or an array.')
   }
 
+  
   if(Array.isArray(actionType[0])) {
     store.dispatch(Object.assign({type: actionType[0][0]},  actionType[0][1]))
+  }
+  else if(action.initPayload) {
+    store.dispatch(Object.assign({type: actionType[0]},  action.initPayload))
   } else {
     store.dispatch({type: actionType[0]})
   }
 
   action.promise.then(payload => {
     store.dispatch({type: actionType[1], payload})
+    if(action.done) action.done(payload)
   }).catch(error => {
     store.dispatch({type: actionType[2], error})
   })
